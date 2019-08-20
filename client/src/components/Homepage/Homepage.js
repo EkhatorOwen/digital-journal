cd5aac7b3a87e7dea3fed933a8864155fc590d05
Owen Ekhator <owenekhator@rocketmail.com>
2019-08-20 02:41:09 -0500

    added homepage component

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, Row, Form, FormGroup, Label, Input} from 'reactstrap';
+
+const Homepage = () => {
+  return (
+    <div>
+      <Container>
+        <div>
+          <h3>Create a Note</h3>
+        </div>
+        <div> 
+          <FormGroup>
+          
+          </FormGroup>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Homepage;
