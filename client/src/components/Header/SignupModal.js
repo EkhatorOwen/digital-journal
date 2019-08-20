95702f05956a1972d5a633edcde7ace9fb930ac9
Owen Ekhator <owenekhator@rocketmail.com>
2019-08-20 01:46:33 -0500

    finished signup component

--- client/src/components/Header/SignupModal.js
+++ client/src/components/Header/SignupModal.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Col,
+  Row
+} from 'reactstrap';
 
 const SignupModal = ({toggleOpen, isOpen}) => {
   return (
     <div>
-      <Modal isOpen={isOpen} toggle={toggleOpen}>
-        <ModalHeader toggle={toggleOpen}>
+      <Modal isOpen={isOpen} toggle={() => toggleOpen ('signup')}>
+        <ModalHeader toggle={() => toggleOpen ('signup')}>
           Sign Up
         </ModalHeader>
         <Form>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <FormGroup>
+              <Label for="username">Username:</Label>
+              <Input
+                type="name"
+                name="username"
+                id="username"
+                placeholder="Enter Username"
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password:</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter Password"
+                required
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="comfirm password">Confirm Password:</Label>
+              <Input
+                type="password"
+                name="comfirm password"
+                id="confirmPassword"
+                placeholder="Enter Password"
+                required
+              />
+            </FormGroup>
           </ModalBody>
+          <Container><Row><Col sm="12" md={{ size: 6, offset: 3 }}><p>this is an error message</p></Col></Row></Container>
           <ModalFooter>
-            <Button color="primary" onClick={toggleOpen}>
-              Do Something
+            <Button color="danger" onClick={() => toggleOpen ('signup')}>
+              Cancel
             </Button>
             {' '}
-            <Button color="secondary" onClick={toggleOpen}>
-              Cancel
+            <Button color="primary">
+              Sign Up
             </Button>
           </ModalFooter>
         </Form>
