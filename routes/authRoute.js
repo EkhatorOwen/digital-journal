27b5c57e6f6e01dc13093ccaa44ff174fa9c1b12
Owen Ekhator <owenekhator@rocketmail.com>
2019-09-06 01:41:47 -0500

    added route to get auth user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,43 +1,77 @@
-const User = require('../models/User');
-module.exports = app=> {
-   
-  app.get('/api/login',(req,res)=>{
-    const {username, password} = req.body;
-    console.log(username)
-      User.findOne({username})
-          .then(resp=>{
-            if(resp){
-              console.log(resp)
-            } else {
-              res.status(401).json({errMessage: 'user not found'})
-            }
-          })
-  }),
+const User = require ('../models/User');
+const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken');
+const checkToken = require('../middleware/checkToken');
 
-  app.post('/api/signup',(req,res)=>{
+module.exports = app => {
+  //login route
+  app.post ('/api/login', (req, res) => {
     const {username, password} = req.body;
-      User.findOne({username})
-          .then(resp=>{
-            if (resp){
-               res.status(202).json({errMessage: 'User already exists'});
-               return
-            }
+    User.findOne ({username}).then (resp => {
+      if (resp) {
+        if (bcrypt.compareSync (password, resp.password)) {
+          req.session.user = resp;
+          jwt.sign({resp},process.env.JWTSECRET, (err, token)=>{
+            req.session.token = token;
+            res.status (200).json ({type:'success', resp, token});
           })
-          .catch(err=>{
-            res.status(500).json({errMessage: 'something went wrong'});
-          })
-        
-      const newUser = new User({
-        username,
-        password
-      })  
+        } else {
+          res.send ({type:'err', message:'Wrong username or password'});
+        }
+      } else {
+        res.send ({type: 'err', message: 'user not found'});
+      }
+    });
+  }), 
+  //signup route
+  app.post ('/api/signup', (req, res) => {
+    const {username, password} = req.body;
+    User.findOne ({username})
+      .then (resp => {
+        if (resp) {
+          //check if username exists
+          res.send ({type:'error', message: 'User already exists'});
+          return;
+        }
+      })
+      .catch (err => {
+        res.status (500).json ({type: 'error', message: 'something went wrong'});
+      });
 
-      newUser
-          .save()
-          .then(resp=>{
-            req.session.username=username;
-            res.status(200).json({message:"User successfully saved"})
-          })
+    //this says how much salt is required
+    let salt = bcrypt.genSaltSync (10);
+
+    let hash = bcrypt.hashSync (password, salt);
+
+    const newUser = new User ({
+      username,
+      password: hash,
+    });
+
+    newUser.save ().then (resp => {
+      req.session.user = resp;
+      jwt.sign({resp},process.env.JWTSECRET, (err, token)=>{
+        req.session.token = token;
+        res.status (200).json ({type:'success',resp, token});
+      })
+    });
+  });
+
+  app.post('/api/logout',checkToken,(req,res)=>{
+    jwt.verify (req.session.token, process.env.JWTSECRET, (err, authData) => {
+      if (err) {
+        res.sendStatus (403);
+      } else {
+        delete req.user;
+        delete req.session.user;
+        delete req.session.token;
+        res.json ({success: 'success'});
+      }
+    });
   })
-   
-}
+
+  //check if there is a logged in user in the session
+    app.post('/api/getuser',checkToken,(req,res)=>{
+        
+    })
+};
