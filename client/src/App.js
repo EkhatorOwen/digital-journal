2b293e3bb28d896b8607e06445382a436a51a818
Owen Ekhator <owenekhator@rocketmail.com>
2019-09-07 01:41:19 -0500

    modified app.js to perfrom login, signup and logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios'
+import { useCookies } from 'react-cookie';
+import { useError } from './utils/useError';
 
 import Header from './components/Header/Header.js';
 import {Container} from 'reactstrap';
@@ -11,6 +13,31 @@ function App () {
   const [username, setUsername] = useState ('');
   const [password, setPassword] = useState ('');
   const [confirmPassword, setConfirmPassword] = useState ('');
+  const [errorMsg, setErrormsg] = useState('')
+  const [cookie, setCookie, removeCookie] = useCookies(['auth'])
+  const [isOpen, updateOpen] = useState(false);
+  const [loginModalOpen, updateLoginModal] = useState(false);
+  const [signUpModalOpen, updateSignUpModal] = useState(false);
+
+  useEffect(() => {
+    //check if there is a cookie, then send the cookie to the backend to 
+    axios
+    .get('/api/getuser', {
+        headers: {
+          Authorization: `Bearer ${cookie.auth}`,
+        },
+      })
+         .then(resp=>{ 
+           if(resp.data.type=="error"){
+           return setLoggedIn(false)
+           } else if(resp.data.type=="success"){
+              return  setLoggedIn(true)
+           }
+           return setLoggedIn(false)
+         })
+         .catch(err=>{console.log(err)})
+
+  }, [])
 
   const handleUsernameChange = val => {
     setUsername (val);
@@ -24,17 +51,55 @@ function App () {
     setConfirmPassword (val);
   };
 
+  const logOut = () =>{
+    axios.post('/api/logout',{
+      headers: {
+        Authorization: `Bearer ${cookie.auth}`,
+      },
+    })
+         .then(resp=>{
+           removeCookie('auth')
+         })
+        }
+
   const handleLogin = (e) =>{
     e.preventDefault();
-    axios.post('/api',{username,password})
-         .then(resp=>console.log(resp))
+    axios.post('/api/login'{
+      headers: {
+        Authorization: `Bearer ${cookie.auth}`,
+      },
+    }    ,{username,password})
+         .then(resp=>{
+           if(resp.data.type==='success'){                          
+               setLoggedIn(true)
+              return setCookie('auth',resp.data.token)
+           } 
+          //  else if (resp.data.type==='error'){
+             
+          //  }
+          console.log(resp.data)
+         })
          .catch(err=>console.log(err))
+  }
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if(password!==confirmPassword) {
+      setErrormsg('passwords do not match')
+      return;
+    }
+    axios.post('/api/signup',{username,password})
+          .then(resp=>{
+            setCookie('auth',resp.data.token)
+          })
+          .catch(err=>console.log(err))
   }
 
   const emptyFields = () =>{
     setUsername('');
     setPassword('');
     setConfirmPassword('');
+    setErrormsg('');
   }
 
 
@@ -49,9 +114,19 @@ function App () {
         handleConfirmPasswordChange={handleConfirmPasswordChange}
         emptyFields={emptyFields}
         handleLogin={handleLogin}
+        setErrormsg={setErrormsg}
+        errorMsg={errorMsg}
+        handleSignup={handleSignup}
+        isOpen={isOpen}
+        updateOpen={updateOpen}
+        loginModalOpen={loginModalOpen}
+        updateLoginModal={updateLoginModal}
+        signUpModalOpen={signUpModalOpen}
+        updateSignUpModal={updateSignUpModal}
+
       />
       <Container>
-        {isLoggedIn ? <WelcomeBadge /> : <Homepage handleLogin={handleLogin} />}
+        {isLoggedIn ? <Homepage /> : <WelcomeBadge /> }
       </Container>
     </div>
   );
