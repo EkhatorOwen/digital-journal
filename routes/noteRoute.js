ca97502085a0b23b83689b2a1618b5e0013bebce
Owen Ekhator <owenekhator@rocketmail.com>
2019-09-09 05:48:35 -0500

    added createnote route

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,8 +1,32 @@
-const bcrypt = require ('bcrypt');
 const jwt = require ('jsonwebtoken');
 const checkToken = require('../middleware/checkToken');
+const Note = require('../models/Note');
+const User = require('../models/User');
+
 
 
 module.exports = app =>{
+  app.post('/api/createnote',checkToken,(req,res)=>{
+    const {title, body, userId} = req.body;
 
+    jwt.verify(req.session.token, process.env.JWTSECRET, (err, authData) =>{
+      if(err){
+        res.sendStatus(405);
+      } else {
+        const newNote = new Note({
+          title,
+          body,
+          user: userId
+        })
+        newNote.save()
+               .then(resp=>{
+                 console.log(resp)
+               })
+
+               res.status (200).json ({type:'success',message: "note successfully saved"});
+
+      }
+    })
+  })
+     
 }
