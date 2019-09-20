175ce3d2af6996bcf5ace92eb8a55655ea25fc5b
Owen Ekhator <owenekhator@rocketmail.com>
2019-09-19 22:47:27 -0500

    modified component to allow for editing and deletion

--- client/src/components/Homepage/CardComponent.js
+++ client/src/components/Homepage/CardComponent.js
@@ -7,17 +7,16 @@ import {
   Button,
 } from 'reactstrap';
 
-const CardComponent = ({cardTitle, cardBody, focusOnTitle}) => {
+const CardComponent = ({cardTitle, cardBody,  editCard, ele, deleteNote}) => {
   return (
     <div>
       <Card>
-        
         <CardBody>
           <CardTitle>{cardTitle || "not provided yet"}</CardTitle>
           <CardText>
            {cardBody || "to be provided"}
           </CardText>
-          <Button onClick={focusOnTitle}>Edit</Button> <Button>Delete</Button>
+          <Button onClick={()=>editCard(ele)}>Edit</Button> <Button onClick={()=>deleteNote(ele)}>Delete</Button>
         </CardBody>
       </Card>
     </div>
