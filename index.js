0415a3f1727da2709fa7bb843760274023db8d89
Owen Ekhator <owenekhator@rocketmail.com>
2019-09-25 22:45:44 -0500

    options

--- index.js
+++ index.js
@@ -13,12 +13,23 @@ const authRoute = require(`./routes/authRoute`);
 const noteRoute = require(`./routes/noteRoute`);
 
 const app = express();
-app.set('etag',false);
+app.set('ETag',false);
 
 if (process.env.NODE_ENV === "production") {
+  var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
 
   //https://stackoverflow.com/questions/18811286/nodejs-express-cache-and-304-status-code
-  app.use(express.static(__dirname + "/client/build"));
+  app.use(express.static(__dirname + "/client/build"),options);
   
   const path = require("path");
   app.get("*", (req, res) => {
